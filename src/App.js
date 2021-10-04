9732b90b27c17ffe408682701a9bf917e6208937
Pedro <plsg2020.ufmg@gmail.com>
2021-10-04 09:10:07 -0300

    Componentização
    Foram criados alguns componentes a fim de praticar as formas com que podem ser construídos e usados.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React from 'react';
+import NewComponente from './HelloWorld';
 
+/* O React permite desenvolver separadamente os componentes 
+ * de uma pagina web. Ao executar uma acao em um desses componentes,
+ * nao eh necessario recarregar a pagina toda, soh ele.
+ * Componentes PAI contem componentes FILHO.
+ * Ha 2 maneiras de criar um componente:
+ *  - Funcao JS
+ *  - Classe JS
+*/
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Title></Title>
+      <Title/> 
+      <NewComponente></NewComponente>
     </div>
   );
+}
+
+//Novo componente
+function Title(){
+  return <h1>Hello World</h1>;
 }
 
 export default App;
---
+++ src/Subtitle.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Subtitle(){
+    return(<h2>Subtítulo</h2>);
+}
