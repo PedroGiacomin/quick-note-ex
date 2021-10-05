f5461f134452dd4d57a5d447deb38edcd1da71f6
Pedro <plsg2020.ufmg@gmail.com>
2021-10-05 11:27:42 -0300

    Props
    Passagem de valores para um componente usando props. Exemplo usando children tb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React from "react"
+import React from "react";
+import Membro from "./Membro";
+import Title from "./Title";
+
+const braga = {
+  nome: "Arthur Braga",
+  idade: 19,
+  cargo: "Gerente de Produtos",
+  curso: "Engenharia de Sistemas",
+};
+
+const lima = {
+  nome: "Arthur Lima",
+  idade: 19,
+  cargo: "Gerente Scrum",
+  curso: "Engenharia Elétrica",
+};
 
 function App(){
 
   return(
     <div>
-      <h1>
-        Title
-      </h1>
+      <Title title={"Membros"}>
+        <Membro membro={braga}/>
+        <Membro membro={lima}/>
+      </Title>
     </div>
   )
 
