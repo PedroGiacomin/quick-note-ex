81dda6ff03a32beb1a3a7490b7baa7a3a74ccd8f
Pedro <plsg2020.ufmg@gmail.com>
2021-10-05 15:17:54 -0300

    UseState usando um objeto JSON
    Formulario mais cmpleto, criando um objeto Mmebro JSON

--- src/App.js
+++ src/App.js
@@ -8,31 +8,103 @@ function App() {
   //ja atribuimos cada um deles a uma variavel
 
   //useState permite usar e trocar o valor das variáveis dinamicamente
-  const [nome, setNome] = useState();
-  const [cargo, setCargo] = useState();
+  const [membro, setMembro] = useState({});
+
+  function handleInputChange(e){
+    const key = e.target.name;
+
+    const newMembro = {...membro};
+    newMembro[key] = e.target.value;
+
+    setMembro(newMembro);
+  }
 
   return (
     <div>
       <h1>Novo Membro</h1>
 
-      {/*onChange captura os eventos que ocorrem no input*/}
+      {/*onChange captura os eventos que ocorrem no input
+        ele recebe uma funcao*/}
       
-      <input type="text" placeholder="Nome" onChange={(e) => { setNome(e.target.value) }} />
+      <input 
+        type="text" 
+        placeholder="Nome" 
+        name="nome"
+        onChange={handleInputChange}/>
       <br />
       
-      <input id="GS" type="radio" name="cargo" value="Gerente Scrum"
-        onChange={(e) => {setCargo(e.target.value)}}/>
+      <input 
+        id="GS" 
+        type="radio" 
+        name="cargo" 
+        value="Gerente Scrum"
+        onChange={handleInputChange}/>
       <label htmlFor="GS">Gerente Scrum</label>
       <br />
 
-      <input id="GP" type="radio" name="cargo" value="Gerente de Produtos" 
-        onChange={(e) => {setCargo(e.target.value)}}/>
+      <input 
+        id="GP" 
+        type="radio" 
+        name="cargo" 
+        value="Gerente de Produtos"
+        onChange={handleInputChange}/>
       <label htmlFor="GP">Gerente de Produtos</label>
+      <br/>
+      
+      <input
+        type="text"
+        placeholder="Idade"
+        name="idade"
+        onChange={handleInputChange}/>
+      <br/>
+
+      <input
+        type="text"
+        placeholder="Curso"
+        name="curso"
+        onChange={handleInputChange}/>
+      
+      <input
+        id="diurno"
+        type="radio"
+        name="turno"
+        value="Diurno"
+        onChange={handleInputChange}/>
+      <label htmlFor="diurno">Diurno</label>
+
+      <input
+        id="noturno"
+        type="radio"
+        name="turno"
+        value="Noturno"
+        onChange={handleInputChange}/>
+      <label htmlFor="noturno">Noturno</label>
+      <br/>
+
+      <input
+        type="text"
+        placeholder="Estado"
+        name="estado"
+        onChange={handleInputChange}/>
+      <br/>
+
+      <input
+        type="text"
+        placeholder="Cidade"
+        name="cidade"
+        onChange={handleInputChange}/>
+      <br/>
+      
       <br />
       <br />
 
-      <p>Nome do membro: {nome}</p>
-      <p>Cargo do membro: {cargo}</p>
+      <p>Nome do membro: {membro.nome}</p>
+      <p>Cargo do membro: {membro.cargo}</p>
+      <p>Idade: {membro.idade}</p>
+      <p>Curso: {membro.curso}</p>
+      <p>Turno: {membro.turno}</p>
+      <p>Estado: {membro.estado}</p>
+      <p>Cidade: {membro.cidade}</p>
     </div>
   )
 }
