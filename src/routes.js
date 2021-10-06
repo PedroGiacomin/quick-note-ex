8f621cec7b30cbb3bbeb0fee7463082f8a7dc5d9
Pedro <plsg2020.ufmg@gmail.com>
2021-10-06 14:40:59 -0300

    Criei as rotas
    Criadas rotas para as 3 páginas iniciais da aplicação. A lógica das rotas se encontra no routes.js

---
+++ src/routes.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+//^^^^Biblioteca de terceiros instalada para cuidar das rotas^^^^//
+
+import Cadastro from "./Pages/Cadastro";
+import Login from "./Pages/Login";
+import Home from "./Pages/Home";
+
+//>>>>> ROTAS <<<<<//
+/** 
+ * As rotas sao os caminhos para cada pagina do sistema web,
+ * elas sao o que fica na barra de navegacao do navegador quando
+ * estamos em certa pagina.
+ *   
+ * Cada componente Route recebe dois parametros:
+ *      - path: o que vai ser colocado no navegador pra ir praquela pagina
+ *      - component: qual componente, criado por nos, vai renderizar quando estiver nessa rota
+ */
+
+function Routes(){ 
+
+    //Estrutura das rotas
+    return(
+        <BrowserRouter>
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/cadastro" component={Cadastro}/>
+                <Route path="/home" component={Home}/>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes;
