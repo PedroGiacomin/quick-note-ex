233a17bf44c93099da976609e810847e38f90543
Pedro <plsg2020.ufmg@gmail.com>
2021-10-11 11:31:01 -0300

    HTML e CSS da pag de login
    Exemplo de uso da biblioteca react-bootstrap; flexbox, alinhamento e estilo.

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,12 +1,26 @@
 import React from "react";
-import {Link} from "react-router-dom"
+import {Form, Button} from "react-bootstrap";
+import "./Login.css"
 
 function Login(){
 
     return (
-        <div>
-            <h1>Login</h1>
-            <Link to="home">Home</Link>
+        <div className="base">
+            <div className="caixa">
+                <div>
+                    <img className="logo" src="/images/Logo.png" alt="Life Jóias"/>
+
+                    <Form className="inputs">
+                        <Form.Group className="mb-3" controlId="email">
+                            <Form.Control type="email" placeholder="Email" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="password">
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Button variant="primary">Login</Button>{' '}
+                    </Form>
+                </div>
+            </div>
         </div>
     )
 }
