c73ba301e185e6c8cad93cda0c6d5b965d86e047
Pedro <plsg2020.ufmg@gmail.com>
2021-10-11 14:20:08 -0300

    Login funcional
    Usando useState para salvar as informações de login e uma função para validá-las.

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import {Form, Button} from "react-bootstrap";
+import { useHistory } from "react-router";
 import "./Login.css"
 
 function Login(){
 
-    return (
-        <div className="base">
-            <div className="caixa">
-                <div>
-                    <img className="logo" src="/images/Logo.png" alt="Life Jóias"/>
+  const [email, setEmail] = useState();
+  const [senha, setSenha] = useState();
+  const history = useHistory();
 
-                    <Form className="inputs">
-                        <Form.Group className="mb-3" controlId="email">
-                            <Form.Control type="email" placeholder="Email" />
-                        </Form.Group>
-                        <Form.Group className="mb-3" controlId="password">
-                            <Form.Control type="password" placeholder="Senha" />
-                        </Form.Group>
-                        <Button variant="primary">Login</Button>{' '}
-                    </Form>
-                </div>
-            </div>
-        </div>
-    )
+  //Para teste de validacao
+  const rightEmail = "lampinho@cpejr.com.br"
+  const rightSenha = "lampejavrau"
+
+  function login(){
+      if(email === rightEmail && senha === rightSenha){
+        history.push("home");
+        alert("Bem vindo! " + email);
+      }
+      else{
+        alert("Senha e/ou Email incorretos");
+      }
+  }
+
+  return (
+    <div className="base"> 
+      <div className="caixa">
+          <div>
+            <img className="logo" src="/images/Logo.png" alt="Life Jóias"/>
+
+            <Form className="inputs">
+              <Form.Group className="mb-3" controlId="email">
+                <Form.Control type="email" placeholder="Email" 
+                  onChange={(e) => {setEmail(e.target.value)}}/>
+              </Form.Group>
+
+              <Form.Group className="mb-3" controlId="password">
+                <Form.Control type="password" placeholder="Senha" 
+                  onChange={(e) => {setSenha(e.target.value)}}/>
+              </Form.Group>
+
+              <Button variant="primary" onClick={() => login()}>Login</Button>{' '}
+            </Form>
+          </div>
+      </div>
+    </div>
+  )
 }
 
 export default Login;
