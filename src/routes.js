df0443a31c94308df232f2272d81e050face4d96
Pedro <plsg2020.ufmg@gmail.com>
2021-10-12 10:30:34 -0300

    Menu de navegação
    Criei o Menu usando o componente List da MUI. Ainda não está funcional. Exemplos de: bibliotecas MUI e icon; uso do map; uso de um vetor de objetos; navegação por rotas

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom"
 import Cadastro from "./Pages/Cadastro";
 import Login from "./Pages/Login";
 import Home from "./Pages/Home";
+import Menu from "./Pages/Menu";
 
 //>>>>> ROTAS <<<<<//
 /** 
@@ -30,6 +31,7 @@ function Routes(){
         <Route path="/login" component={Login}/>
         <Route path="/cadastro" component={Cadastro}/>
         <Route path="/home" component={Home}/>
+        <Route path="/temp" component={Menu}/>
       </Switch>
     </BrowserRouter>
   )
